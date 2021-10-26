c8fd72d35ce257f56d1af1762e44aa086f0d1a15
andreeabejinariu0 <andreea.bejinariu@stud.ubbcluj.ro>
2021-10-26 15:17:05 +0300

    'lab5'

---
+++ Domain/obiect.py
@@ -0,0 +1,30 @@
+
+lista_obiecte = []
+
+def get_all_object():
+    return lista_obiecte
+
+def getNewObject(_id: int, _nume: str, _descriere: str, _pret: int, _locatie: str):
+    obiect = {
+        'id': _id,
+        'nume': _nume,
+        'descriere': _descriere,
+        'pret': _pret,
+        'locatie': _locatie
+    }
+    return obiect
+
+def get_id(obiect):
+    return obiect['id']
+
+def get_name(obiect):
+    return obiect['nume']
+
+def get_description(obiect):
+    return obiect['descriere']
+
+def get_price(obiect):
+    return obiect['pret']
+
+def get_location(obiect):
+    return obiect['locatie']
---
+++ main.py
@@ -0,0 +1,6 @@
+
+from Domain.obiect import getNewObject, get_name, get_price
+
+obiect = getNewObject(1,'birou','primul obiect', 150, 'Cluj')
+print(get_price(obiect))
+print(get_name(obiect))
