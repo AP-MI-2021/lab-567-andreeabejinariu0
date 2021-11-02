f605c238dbeee67563cfd3680aa9e37cd26c19ff
andreeabejinariu0 <andreea.bejinariu@stud.ubbcluj.ro>
2021-11-02 20:57:36 +0200

    'erori'

--- Logic/crud.py
+++ Logic/crud.py
@@ -2,15 +2,21 @@ from Domain.obiect2 import getNewObject, get_id
 
 
 def create(lista_obiecte: list, _id: int, _nume: str, _descriere: str, _pret: int, _locatie: str):
+    if read(lista_obiecte, _id) is not None:
+        raise ValueError(f'Exista deja un obiect cu id-ul {_id}!')
+
+    if len(_locatie) != 4:
+        raise ValueError(f'Locatie introdusa gresit! Trebuie sa aiba exact 4 caractere! ')
+
     obiect = getNewObject(_id, _nume, _descriere, _pret, _locatie)
     # lista_obiecte.append(obiect)
     # return lista_obiecte
+
     return lista_obiecte + [obiect]
 
 
 def read(lista_obiecte: list, id_obiect: int = None):
     obiect_gasit = None
-
     if id_obiect is None:
         return lista_obiecte
 
@@ -22,6 +28,8 @@ def read(lista_obiecte: list, id_obiect: int = None):
 
 
 def update(lista_obiecte: list, new_object):
+    if read(lista_obiecte, get_id(new_object)) is None:
+        raise ValueError(f'Nu exista niciun obiect cu id-ul {get_id(new_object)}!')
     result = []
 
     for obiect in lista_obiecte:
@@ -33,6 +41,8 @@ def update(lista_obiecte: list, new_object):
     return result
 
 def delete(lista_obiecte, id_obiect: int):
+    if read(lista_obiecte, id_obiect) is None:
+        raise ValueError(f'Nu exista niciun obiect cu id-ul {id_obiect}!')
     result = []
 
     for obiect in lista_obiecte:
